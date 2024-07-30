6cd112edc3d93ee19ae484cf1a013a6c0aea2578
Shilpa <shilpaw2210@gmail.com>
2024-07-30 20:47:54 +0530

    testng

--- src/test/TestNG.java
+++ src/test/TestNG.java
@@ -12,9 +12,20 @@ public class TestNG {
 		System.out.println("Hello");
 	}
 	@Test
+	public void display()
+	{
+		System.out.println("How are you");
+	}
+	@Test
 	public void Test1()
 	{
+		//Demo 
 		System.out.println("Bye");
+		
+		
+//		Demo
+//		Demo
+//		Demo
 	}
 
 }
